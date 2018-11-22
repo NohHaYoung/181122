fa3d02a564959550fbd1538347d4e87b392a065f
NohHaYoung <42965065+NohHaYoung@users.noreply.github.com>
2018-11-22 15:08:16 +0900

    1

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char src[100] = "programming course";
+	char dst[100];
+	
+	/*src의 내용을 dst로 옮김*/
+	strcpy(dst, src);
+	
+	printf("%s\n", dst);
+	
+	return 0;
+}
